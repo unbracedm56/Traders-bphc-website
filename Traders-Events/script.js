1ce8950f2cd0f94181aff34d7ca53889f32ca964
unbracedm56 <10c09bharghav@gmail.com>
2025-02-18 00:25:57 +0530

    Delete Traders-Events directory

--- Traders-Events/script.js
+++
@@ -1,7 +0,0 @@
-function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-}
-
-getYear();
